04b565a99ce62c8996b9738929a229ca56488ebc
Phodal Huang <h@phodal.com>
2014-07-23 21:14:00 +0800

    Add a blog template

--- HomeView.js
+++ HomeView.js
@@ -3,14 +3,40 @@ define([
     'underscore',
     'mustache',
     'text!/index.html',
-    'json!/configure.json'
-], function($, _, Mustache, indexTemplate, configure) {
+    'text!/blog.html'
+], function($, _, Mustache, indexTemplate, blogTemplate) {
+
+    var BlogPostModel = Backbone.Model.extend({
+        name: 'Blog Posts',
+        url: function(){
+            return this.instanceUrl;
+        },
+        initialize: function(props){
+            this.instanceUrl = props;
+        }
+    });
 
     var HomeView = Backbone.View.extend({
         el: $('#aboutArea'),
 
-        render: function() {
-            this.$el.html(Mustache.to_html(indexTemplate, configure));
+        initialize: function(){
+            this.getBlog();
+        },
+
+        getBlog: function() {
+            var url = '/1.json';
+            var that = this;
+            collection = new BlogPostModel;
+            collection.initialize(url);
+            collection.fetch({
+                success: function(collection, response){
+                    that.render(response);
+                }
+            });
+        },
+
+        render: function(response) {
+            this.$el.html(Mustache.to_html(blogTemplate, response));
         }
     });
 
